e7aacbb9c3f68dcb214a539b781a7d7818b37ad2
Aarushi-Srivastava-07 <aarushisrivastava07@gmail.com>
2026-03-08 15:04:38 +0530

    Merge Problem2

--- src/Week_1_2_Problems.java
+++ src/Week_1_2_Problems.java
@@ -1,59 +1,7 @@
-package problem1;
-
-// import java.util.*;
-
-// public class Week_1_2 {
-//     private Map<String, Integer> userDatabase = new HashMap<>();
-//     private Map<String, Integer> attemptFrequency = new HashMap<>();
-
-//     public Week_1_2() {
-//         userDatabase.put("john_doe", 101);
-//         userDatabase.put("jane_smith", 102);
-//         userDatabase.put("admin", 103);
-//     }
-
-//     public boolean checkAvailability(String username) {
-//         attemptFrequency.put(username, attemptFrequency.getOrDefault(username, 0) + 1);
-//         return !userDatabase.containsKey(username);
-//     }
-
-//     public List<String> suggestAlternatives(String username) {
-//         List<String> suggestions = new ArrayList<>();
-//         for (int i = 1; i <= 3; i++) {
-//             suggestions.add(username + i);
-//         }
-//         suggestions.add(username.replace("_", "."));
-//         suggestions.add(username + "_");
-//         suggestions.removeIf(userDatabase::containsKey);
-//         return suggestions;
-//     }
-
-//     public String getMostAttempted() {
-//         return attemptFrequency.entrySet()
-//                 .stream()
-//                 .max(Map.Entry.comparingByValue())
-//                 .map(Map.Entry::getKey)
-//                 .orElse("No attempts yet");
-//     }
-
-//     public static void main(String[] args) {
-//         Week_1_2 checker = new Week_1_2();
-
-//         System.out.println("checkAvailability(\"john_doe\"): " + checker.checkAvailability("john_doe"));
-//         System.out.println("checkAvailability(\"new_user\"): " + checker.checkAvailability("new_user"));
-//         System.out.println("checkAvailability(\"john_doe\"): " + checker.checkAvailability("john_doe"));
-//         System.out.println("checkAvailability(\"admin\"): " + checker.checkAvailability("admin"));
-
-//         System.out.println("Suggestions for 'john_doe': " + checker.suggestAlternatives("john_doe"));
-//         System.out.println("Most attempted: " + checker.getMostAttempted());
-//     }
-// }
-
-
 import java.util.*;
 import java.util.concurrent.*;
 
-public class Week_1_2 {
+public class Week_1_2_Problems {
 
     private final ConcurrentHashMap<String, Product> products = new ConcurrentHashMap<>();
 
@@ -101,8 +49,8 @@ public class Week_1_2 {
         else return "Added to waiting list, position #" + (-result);
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        InventoryManager manager = new InventoryManager();
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        Week_1_2_Problems manager = new Week_1_2_Problems();
         String productId = "IPHONE15_256GB";
         manager.addProduct(productId, 100);
 
